9278a9d15b517750302daaf33d0069c33dcaec1a
TOURE SOULEYMANE <toure5013@gmail.com>
2021-01-22 17:40:32 +0000

    fixe number algorithm done

--- example/example_mobile.js
+++ example/example_mobile.js
@@ -1,16 +1,28 @@
 const pnn10 =  require("../index");
 
 /**
- * The number can be this format
+ * For mobile number (with - or . or space) : 
  * xxxxxxxx
  * +225xxxxxxxx
  * (+225)xxxxxxxx
  * (00225)xxxxxxxx
  * (00225)xxxxxxxx
  */
+
 var converted_number =  pnn10.phone_number("(+225)08 - 17 - 56 - 31");
 console.log(converted_number);
 
+
+
+
+/**
+ * For fixe number (with - or . or space) :
+ *  xxxxxxxx
+ * +225xxxxxxxx
+ * (+225)xxxxxxxx
+ * (00225)xxxxxxxx
+ * (00225)xxxxxxxx
+ */
 
 var converted_fixe_number =  pnn10.fixe_number("20 30 09 09");
 console.log(converted_fixe_number);
