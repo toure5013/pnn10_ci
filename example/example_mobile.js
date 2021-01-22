1291406c0716a6f557a85a8cccb78106248a4603
TOURE SOULEYMANE <toure5013@gmail.com>
2021-01-22 18:42:06 +0000

    update example

--- example/example_mobile.js
+++ example/example_mobile.js
@@ -2,11 +2,20 @@ const pnn10 =  require("../index");
 
 /**
  * For mobile number (with - or . or space) : 
- * xxxxxxxx
- * +225xxxxxxxx
- * (+225)xxxxxxxx
- * (00225)xxxxxxxx
- * (00225)xxxxxxxx
+
+- xxxxxxxx
+- +225xxxxxxxx
+- (+225)xxxxxxxx
+- (00225)xxxxxxxx
+  
+- xx.xx.xx.xx
+- (+225)xx.xx.xx.xx
+- +225xx.xx.xx.xx
+
+- xx xx xx xx
+- (+225)xx xx xx xx
+- +225 xx xx xx xx
+
  */
 
 var converted_number =  pnn10.phone_number("(+225)08 - 17 - 56 - 31");
@@ -17,11 +26,19 @@ console.log(converted_number);
 
 /**
  * For fixe number (with - or . or space) :
- *  xxxxxxxx
- * +225xxxxxxxx
- * (+225)xxxxxxxx
- * (00225)xxxxxxxx
- * (00225)xxxxxxxx
+- xxxxxxxx
+- +225xxxxxxxx
+- (+225)xxxxxxxx
+- (00225)xxxxxxxx
+  
+- xx.xx.xx.xx
+- (+225)xx.xx.xx.xx
+- +225xx.xx.xx.xx
+
+- xx xx xx xx
+- (+225)xx xx xx xx
+- +225 xx xx xx xx
+
  */
 
 var converted_fixe_number =  pnn10.fixe_number("20 30 09 09");
