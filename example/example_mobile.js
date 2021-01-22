585129119fb705348ca8286fd908f420e33e133c
TOURE SOULEYMANE <toure5013@gmail.com>
2021-01-22 19:33:18 +0000

    convertion for uknow number type added

--- example/example_mobile.js
+++ example/example_mobile.js
@@ -19,7 +19,7 @@ const pnn10 =  require("../index");
  */
 
 var converted_number =  pnn10.phone_number("(+225)08 - 17 - 56 - 31");
-console.log(converted_number);
+// console.log(converted_number);
 
 
 
@@ -42,4 +42,29 @@ console.log(converted_number);
  */
 
 var converted_fixe_number =  pnn10.fixe_number("20 30 09 09");
-console.log(converted_fixe_number);
+// console.log(converted_fixe_number);
+
+
+
+
+
+
+/**
+ * For both fixe number Or mobile Number (with - or . or space) :
+- xxxxxxxx
+- +225xxxxxxxx
+- (+225)xxxxxxxx
+- (00225)xxxxxxxx
+  
+- xx.xx.xx.xx
+- (+225)xx.xx.xx.xx
+- +225xx.xx.xx.xx
+
+- xx xx xx xx
+- (+225)xx xx xx xx
+- +225 xx xx xx xx
+
+ */
+
+var converted_both_phone_number_fixe_number =  pnn10.both_phone_number_fixe_number("08175631");
+console.log(converted_both_phone_number_fixe_number);
--- index.js
+++ index.js
@@ -103,6 +103,7 @@ class Pnn10class {
                     error: false,
                     message: "Le numéro " + user_number_string + " est un numéro " + type_network + " valid  ✅",
                     type_network,
+                    numer_type: "mobile",
                     number_converted_national: number_converted_national,
                     number_converted_international: number_converted_international
                 }
@@ -223,6 +224,7 @@ class Pnn10class {
                     error: false,
                     message: "Le numéro " + user_number_string + " est un numéro " + type_network + " valid  ✅",
                     type_network,
+                    numer_type: "fixe",
                     number_converted_national: number_converted_national,
                     number_converted_international: number_converted_international
                 }
@@ -237,6 +239,97 @@ class Pnn10class {
                 number_converted_international
             }
         }
+    }
+
+    both_phone_number_fixe_number(user_number) {
+
+        var is_number_valid = false;
+        var reguex_ivory_coast_number = /^(\(+225\)|\+225|00225|225|)[0-9]+$/
+
+        var reg_orange = /^[3]{1}[0-9]{1}[8]{1}[0-9]+$/; //pour le cas ou le 3eme chiffre est 8 le premier chiffre est 3
+        var reg_orange_1 = /^[1-9]{1}[0-9]{1}[2345679]{1}[0-9]+$/; //3 eme chiffre 2,3,4,5,6,7,9 
+        var reg_mtn = /^[3]{1}[0-9]{1}[0]{1}[0-9]+$/; //Troisième chiffre à 0 :  
+        var reg_moov = /^[2]{1}[0-9]{1}[8]{1}[0-9]+$/; //debute par 2 et le 3eme chiffre est 8 = 
+
+
+        //convert to string
+        var user_number_string = user_number.toString();
+
+        //clean the data by removing (. , - espace)
+        var clean_regex = /[.\-*\(\) ]+/;
+        while (user_number_string.match(clean_regex)) {
+            user_number_string = user_number_string.replace(clean_regex, "")
+        }
+
+
+        var user_number_string_length = user_number_string.length;
+        var user_final_number = ""
+
+        if (user_number_string_length > 8) {
+            //check if it's a valid number
+            var is_ivory_coast_number = reguex_ivory_coast_number.test(user_number_string)
+            var number_of_caracter_to_delete = user_number_string_length - 8;
+
+            if (is_ivory_coast_number) {
+                // console.log("Numero valid ✅")
+                is_number_valid = true;
+            } else {
+                is_number_valid = false;
+            }
+
+            var user_number_height_caracter = [];
+            var caracter_to_delete;
+            if (is_number_valid) {
+                for (var i = 0; i < user_number_string_length; i++) {
+                    if (i >= number_of_caracter_to_delete) {
+                        user_number_height_caracter.push(user_number_string[i]);
+                    } else {
+                        if (i == 0) {
+                            caracter_to_delete = user_number_string[i];
+                        } else {
+                            caracter_to_delete = caracter_to_delete + "" + user_number_string[i];
+
+                        }
+                    }
+                }
+                //delete the indicatifs and keep 8 caracter
+                user_final_number = user_number_string.replace(caracter_to_delete, "");
+            }
+        } else if (user_number_string.length == 8) {
+            // console.log("Numero valid  ✅  à  8️⃣  chiffre")
+            user_final_number = user_number_string.toString();
+            is_number_valid = true;
+
+        } else {
+            // console.log("Ceci n'est pas un numéro valid")
+            is_number_valid = false;
+        }
+        //stock user new number information
+        var type_network = ""
+        var number_converted_national = ""
+        var number_converted_international = ""
+       
+        if(is_number_valid){
+            //we check here if it's fixed fphone number or mobile phone number
+            var regex_fixe_number = /^[2-3]{1}[0-9]{7}$/
+            var is_fixe_number = regex_fixe_number.test(user_number_string);
+            var final_result = ""
+            if(is_fixe_number){
+                 final_result = this.fixe_number(user_number_string);
+            }else{
+                final_result = this.phone_number(user_number_string);
+            }
+            return final_result;
+        }else{
+            return {
+                error: true,
+                message: "🛑  Le numéro " + user_number_string + ", n'est pas un numéro valid",
+                type_network,
+                number_converted_national,
+                number_converted_international
+            }
+        }
+
     }
 }
 
